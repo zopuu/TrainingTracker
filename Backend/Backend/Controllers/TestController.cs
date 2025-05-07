c23c82da0ba9027e5e8aa40f247a827311094bd0
zopuu <puzovicveljko02@gmail.com>
2025-05-07 19:47:47 +0200

    Initial projects setup

---
+++ Backend/Backend/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Backend.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : Controller {
+        
+        [HttpGet]
+        public IActionResult Test() {
+            return Ok("This is a test. Standby!!");
+        }
+    }
+}

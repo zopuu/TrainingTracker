660de533cd22bb7738d2a61cc077e770c52d64b9
zopuu <puzovicveljko02@gmail.com>
2025-05-08 13:06:55 +0200

    feat: implemented initial layout

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
